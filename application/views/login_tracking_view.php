d78222dc858847e2e917b5503eaa117b8310de13
dimasgung <dimasgung@gmail.com>
2020-05-29 00:00:07 +0700

    Tambah LoginTracking Controller dan View

---
+++ application/views/login_tracking_view.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Login Tracking</title>
+  </head>
+  <body>
+    <h1>Welcome To Login Tracking</h1>
+  </body>
+</html>
