fe12cec9200f302633e2e3d6e97e22e983209b9b
denny yusuf <dimasgung@gmail.com>
2020-06-01 22:42:50 +0700

    delete first table login tracking

--- application/views/login_tracking/login_tracking_view.php
+++ application/views/login_tracking/login_tracking_view.php
@@ -21,36 +21,6 @@
     <!-- Main content -->
     <section class="content">
       <a href="<?php echo site_url('LoginTracking/add'); ?>" class="btn btn-sm btn-success">Add Login Tracking</a>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">User Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">View</th>
-            <th width="200">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-			<?php 
-				$row = 0;
-				foreach ($logintracking as $logintracking_item): 
-					$row++ ?>
-		          <tr>
-		            <th scope="row"><?php echo $row?></th>
-		            <td><?php echo $logintracking_item['USERID']?></td>
-		            <td><?php echo $logintracking_item['NAME']?></td>
-		            <td>
-		            	<a href="<?php echo site_url('LoginTracking/detail/'.$logintracking_item['LOGINTRACKINGID']); ?>">View detail</a>
-		            </td>
-		            <td>
-		            	<a href="<?php echo site_url('LoginTracking/delete/'.$logintracking_item['LOGINTRACKINGID']);?>" class="btn btn-sm btn-danger">Delete</a>
-		            	<a href="<?php echo site_url('LoginTracking/update/'.$logintracking_item['LOGINTRACKINGID']);?>" class="btn btn-sm btn-info">Update</a>
-                 	</td>
-		          </tr>
-			<?php endforeach; ?>
-        </tbody>
-      </table>
        <div class="container-fluid">
         <div class="row">
           <div class="col-12">
