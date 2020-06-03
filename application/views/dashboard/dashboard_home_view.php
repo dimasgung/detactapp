edca27ef4c3ac9e747fc4ba88b5d144bce795b5d
denny yusuf <dimasgung@gmail.com>
2020-06-04 02:53:08 +0700

    add mock up dashboard

--- application/views/dashboard/dashboard_home_view.php
+++ application/views/dashboard/dashboard_home_view.php
@@ -24,12 +24,75 @@
     <section class="content">
 
       <div class="container-fluid">
+                <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>2</h3>
+
+                <p>Inbox Analytic</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>120</h3>
+
+                <p>Akun Masih Terdeteksi Sharing</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>Akun Belum Konfirmasi Sharing</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>76</h3>
+
+                <p>Akun Belum di Follow Up</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+
         <div class="row">
           <div class="col-12">
             <!-- Default box -->
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Title</h3>
+                <h3 class="card-title">Pengumuman</h3>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
@@ -39,13 +102,114 @@
                 </div>
               </div>
               <div class="card-body">
-                Start creating your amazing application!
+                Untuk setiap akun yang terdeteksi sharing account, direkomendasikan untuk mengubah password. Jika tidak ada respon maka akun bisa disuspend.
               </div>
               <!-- /.card-body -->
               <div class="card-footer">
-                Footer
+                Fraud Cheater Team
               </div>
               <!-- /.card-footer-->
+            </div>
+          </div>
+
+          <div class="col-6">
+            <div class="card">
+              <div class="card-header border-transparent">
+                <h3 class="card-title">Analytic in Progress</h3>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
+                    <thead>
+                    <tr>
+                      <th>Login Tracking History ID</th>
+                      <th>Processed Attemp Date</th>
+                      <th>Aplikasi</th>
+                      <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>2</td>
+                      <td>2020-06-01</td>
+                      <td>NOSSA</td>
+                      <td><span class="badge badge-primary">INPROGRESS</span></td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a>
+                <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
+            <!-- /.card -->
+          </div>
+                    <div class="col-6">
+            <div class="card">
+              <div class="card-header border-transparent">
+                <h3 class="card-title">Analytic Done</h3>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body p-0">
+                <div class="table-responsive">
+                  <table class="table m-0">
+                    <thead>
+                    <tr>
+                      <th>Login Tracking History ID</th>
+                      <th>Processed Attemp Date</th>
+                      <th>Aplikasi</th>
+                      <th>Status</th>
+                      <th>Lihat Hasil</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>2020-05-11</td>
+                      <td>NOSSA</td>
+                      <td><span class="badge badge-sucess">Done</span></td>
+                      <td><a href="<?php echo base_url();?>AnalyticResult" class="btn btn-link">Lihat Hasil</a></td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>2020-05-11</td>
+                      <td>NOSSA</td>
+                      <td><span class="badge badge-sucess">Done</span></td>
+                      <td><a href="<?php echo base_url();?>AnalyticResult" class="btn btn-link">Lihat Hasil</a></td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Order</a>
+                <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All Orders</a>
+              </div>
+              <!-- /.card-footer -->
             </div>
             <!-- /.card -->
           </div>
