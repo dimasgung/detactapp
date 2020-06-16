943c79f1735d1e00463881ed52d61091004052b8
denny yusuf <dimasgung@gmail.com>
2020-06-16 14:25:37 +0700

    minor fixing 10 latest confirmed cases

--- application/views/dashboard/dashboard_home_view.php
+++ application/views/dashboard/dashboard_home_view.php
@@ -193,7 +193,7 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header border-transparent">
-                <h3 class="card-title"><b>10 Latest Confirmation Cases</b></h3>
+                <h3 class="card-title"><b>10 Latest Confirmed Cases</b></h3>
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse">
                     <i class="fas fa-minus"></i>
